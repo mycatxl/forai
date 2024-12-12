4fdb34ab2c43e77d867335b47e97b2acddc26a12
mycatxl <160130398+mycatxl@users.noreply.github.com>
2024-12-12 23:09:51 +0800

    Delete TokenPay_test directory

--- TokenPay_test/Config/Handle.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare (strict_types=1);
-
-return [
-    \Kernel\Plugin\Handle\Pay::class => \App\Plugin\TokenPay\Handle\Pay::class
-];
--- TokenPay_test/Config/Pay.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare (strict_types=1);
-return [
-    'TRX' => 'TRX',
-    'USDT_TRC20' => 'USDT-TRC20',
-    'EVM_ETH_ETH' => 'ETH',
-    'EVM_BSC_BNB' => 'BSC',
-    'EVM_BSC_USDT_BEP20' => 'BSC-USDT'
-];
